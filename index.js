fc7d907b1366765a65454396ad6ab52865525ae7
adnanshafique468 <askkamboa612@gmail.com>
2026-02-21 23:34:35 -0800

    done it

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello Mr. adnan shafiq Jenkins for this CI/CD 🚀</h1>");
+  res.send("<h1>Hello Mr. adnan shafiq Jenkins for this CI/CD for this amazing way 🚀</h1>");
 });
 
 app.listen(3000, () => {
